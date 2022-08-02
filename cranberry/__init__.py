e4dc779a922b708246bacb49690b27d8e1441081
Oskar Niemenoja <osku@Oskar-MacBook-Pro.local>
2022-08-02 12:57:39 +0300

    Restructured project

---
+++ cranberry/__init__.py
@@ -0,0 +1,6 @@
+from cranberry.layer import *
+from cranberry.loss import *
+from cranberry.model import *
+from cranberry.optimizer import *
+from cranberry.tensor import *
+from cranberry.utils import *
--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cranberry',
-    package_dir={"": "src"},
-    packages=find_packages(where='src'),
+    packages=find_packages(),
     version='0.1.0',
     description='The Cranberry Deep Learning library',
     author='Oskar Niemenoja',
